d40d63398008be4fc6bc0279ff7e44cd2f6f3fe5
RamiSwa <118728140+RamiSwa@users.noreply.github.com>
2025-03-21 06:31:11 +0200

    updated 3

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Project, ProjectImage
+from .models import Project, ProjectImage, Technology
 
 class ProjectImageInline(admin.TabularInline):
     model = ProjectImage
@@ -10,3 +10,11 @@ class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'date_created')
     prepopulated_fields = {'slug': ('title',)}
     inlines = [ProjectImageInline]
+    filter_horizontal = ('technologies',)  # Better UI for ManyToMany in admin
+    search_fields = ('title', 'description', 'category')
+    list_filter = ('category', 'technologies', 'date_created')
+
+@admin.register(Technology)
+class TechnologyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'link')
+    search_fields = ('name',)
--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.utils.text import slugify
 
+class Technology(models.Model):
+    """Represents a single technology or tool (e.g., Django, Docker, PostgreSQL)."""
+    name = models.CharField(max_length=100, unique=True)
+    link = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Project(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True, blank=True)
@@ -10,6 +19,12 @@ class Project(models.Model):
     demo_link = models.URLField(blank=True, null=True)
     date_created = models.DateField(auto_now_add=True)
     meta_description = models.CharField(max_length=160, blank=True)
+
+    # ✅ NEW: ManyToMany with Technology
+    technologies = models.ManyToManyField(Technology, related_name="projects", blank=True)
+
+    class Meta:
+        ordering = ['-date_created']
 
     def save(self, *args, **kwargs):
         if not self.slug:
